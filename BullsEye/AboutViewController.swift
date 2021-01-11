c43446b940916e8e46b2151ce99bbbc5ad4246d2
FadyMagdy <77287109+FadyMagdySamy@users.noreply.github.com>
2021-01-11 19:52:39 +0200

    Add files via upload
    initial commit

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  @IBAction func close() {
+    dismiss(animated: true, completion: nil)
+  }
+  
+}
